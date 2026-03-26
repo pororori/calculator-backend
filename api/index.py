56fd16f11d6497aa86634e83a68e21b11ab436ae
pororori <cathy0e0711@gmail.com>
2026-03-26 15:14:03 +0900

    Initialize FastAPI app with CORS middleware

---
+++ api/index.py
@@ -0,0 +1,15 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+@app.get("/add")
+def add(a: float, b: float):
+    return {"result": a + b}
